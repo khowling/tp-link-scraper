df4fd855104697fe07d6617b6ffda55601aa601c
keith <khowling@gmail.com>
2020-10-31 23:11:59 +0000

    add port disable

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 * TP-LINK Managed Switch Prometheus scraper
 
-scrape the port metrics from a TP-LINK managed switch and expose as a target for Prometheus 
+Scrape the port metrics from a TP-LINK managed switch and expose as a target for Prometheus.
+
+** Ubuntu Startup
+
+create script ```start.sh```
+
+```
+SWITCH_MANAGEMENT_URL="http://xxx.xxx.xxx.xxx" SWITCH_MANAGEMENT_STATS="PortStatisticsRpm.htm" LOGIN_FORM_DATA="username=xxxx&password=xxxx&cpassword=&logon=Login" node /home/xxxxx/tp-link-scraper/index.js
+```
+
+create file ```/etc/systemd/system/managed_switch.service```
+
+```
+[Unit]
+Description=ManagedSwitchUI
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=xxxxx
+Group=xxxxx
+Type=simple
+ExecStart=/home/xxxxx/tp-link-scraper/start.sh
+
+[Install]
+WantedBy=multi-user.target
+```
+
+start
+```sudo systemctl [start|status|enable] managed_switch.service```
+
--- index.js
+++ index.js
@@ -68,31 +68,28 @@ function fetch(url, method = 'GET', headers = {}, body) {
     })
 }
 
-async function scrape() {
+async function auth_fetch(url, method, headers, body, retry = false) {
 
-    async function portstats(retry) {
+    const response_html = await fetch(url, method, headers, body)
 
-        const metrics_page = await fetch(`${process.env.SWITCH_MANAGEMENT_URL}/${process.env.SWITCH_MANAGEMENT_STATS}`)
-
-        if (metrics_page.indexOf('logonInfo') > 0) {
-            if (retry === true) {
-                throw new Error(`Failed to login`)
-            } else {
-                console.log(`got the login page`)
-                const login_res = await fetch(`${process.env.SWITCH_MANAGEMENT_URL}/logon.cgi`, 'POST',
-                    { "content-type": "application/x-www-form-urlencoded", "referer": `${process.env.SWITCH_MANAGEMENT_URL}/${process.env.SWITCH_MANAGEMENT_STATS}` },
-                    process.env.LOGIN_FORM_DATA)
-                return await portstats(true)
-            }
+    if (response_html.indexOf('logonInfo') > 0) {
+        if (retry) {
+            throw new Error(`Failed to login`)
         } else {
-            return metrics_page
+            console.log(`got the login page`)
+            const login_res = await fetch(`${process.env.SWITCH_MANAGEMENT_URL}/logon.cgi`, 'POST',
+                { "content-type": "application/x-www-form-urlencoded", "referer": url },
+                process.env.LOGIN_FORM_DATA)
+            return await auth_fetch(url, method, headers, body, true)
         }
+    } else {
+        return response_html
     }
+}
 
-
+async function get_allinfo(url, method = 'GET', headers = {}, body) {
     try {
-
-        const page_data = await portstats(false)
+        const page_data = await auth_fetch(url, method, headers, body)
         const page_data_match = page_data.match(/all_info = ([\s\S]*?);/)
 
         if (page_data_match && page_data_match.length > 0) {
@@ -107,22 +104,105 @@ async function scrape() {
     }
 }
 
+// state 0=disable, 1=enable
+// speed 1 = Auto
+// flowcontrol 0=off, 1=on
+async function disable_port(port, state, speed = 1, flowcontrol = 0) {
+    const disable_res = await auth_fetch(`${process.env.SWITCH_MANAGEMENT_URL}/port_setting.cgi?portid=${port}&state=${state}&speed=${speed}&flowcontrol=${flowcontrol}&apply=Apply`, 'GET', { "referer": `${process.env.SWITCH_MANAGEMENT_URL}/PortSettingRpm.htm` })
+}
+
+
+const PORT_MAPPING = ["hub", "mum_office", "master_bdrm", "dad_office", "living_room", "kitchen", "girl_bdrm", "boy_bdrm"]
+const SPEED = ["Link Down", "Auto", "10MH", "10MF", "100MH", "100MF", "1000MF"]
+const FLOW = ["Off", "On"]
 
 let app = http.createServer(async (req, res) => {
-    // Set a response type of plain text for the response
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    const json_res = await scrape()
+    if (req.url === '/metrics') {
+        // Set a response type of plain text for the response
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        const json_res = await get_allinfo(`${process.env.SWITCH_MANAGEMENT_URL}/${process.env.SWITCH_MANAGEMENT_STATS}`)
 
-    if (json_res.failed) {
-        res.end(json_res)
-    } else {
-        let out = "", idx = 0
-        for (let port of ["router", "mum_office", "master_bdrm", "dad_office", "living_room", "kitchen", "girl_bdrm", "boy_bdrm"]) {
-            out += `managed_switch_TxPkt{room="${port}"} ${json_res.pkts[idx]}` + '\n'
-            out += `managed_switch_RxPkt{room="${port}"} ${json_res.pkts[idx + 2]}` + '\n'
-            idx += 4
+        if (json_res.failed) {
+            res.end(json_res)
+        } else {
+            let out = ""
+            for (let port in PORT_MAPPING) {
+                const pkts_idx = port * 4
+                out += `managed_switch_TxPkt{room="${PORT_MAPPING[port]}"} ${json_res.pkts[pkts_idx]}` + '\n'
+                out += `managed_switch_RxPkt{room="${PORT_MAPPING[port]}"} ${json_res.pkts[pkts_idx + 2]}` + '\n'
+            }
+            res.end(out)
         }
-        res.end(out)
+    } else if (req.url === '/grafana') {
+        res.writeHead(302, { 'Location': `http://${req.headers.host.split(":")[0]}:3000` })
+        res.end()
+
+    } else if (req.url.startsWith('/setport?')) {
+
+        const port_updates = req.url.split('?')[1]
+        console.log(`Updating port : ${port_updates}`)
+        // portid=${port}&state=${state}&speed=${speed}&flowcontrol=${flowcontrol}&apply=Apply
+        const json_res = await get_allinfo(`${process.env.SWITCH_MANAGEMENT_URL}/port_setting.cgi?${port_updates}`, 'GET', { "referer": `${process.env.SWITCH_MANAGEMENT_URL}/PortSettingRpm.htm` })
+        res.writeHead(302, { 'Location': '/' })
+        res.end()
+
+    } else if (req.url === '/') {
+        const json_res = await get_allinfo(`${process.env.SWITCH_MANAGEMENT_URL}/PortSettingRpm.htm`)
+
+        if (json_res.failed) {
+            res.end(json_res)
+        } else {
+            let out = `
+            <html>
+                <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <style>
+                    body{
+                        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                     }
+                     div{
+                        overflow-x: auto;
+                     }
+                     table {
+                        border-collapse: collapse;
+                        border-spacing: 0;
+                        width: 100%;
+                        border: 1px solid rgb(0, 0, 0);
+                     }
+                     th, td {
+                        text-align: left;
+                        padding: 8px;
+                     }
+                     tr:nth-child(even){background-color: #f2f2f2}
+                  </style>
+                </head>
+                <body>
+                    <table>
+                        <tr>
+                            <th>Room (<a href="/grafana">Metrics</a>)</th>
+                            <th>Enabled (toggle)</th>
+                            <th>Speed (actual) </th>
+                            <th>Flow Ctrl (actual)</th>
+                        </tr>`
+
+
+            for (let port in PORT_MAPPING) {
+                out += `
+                <tr>
+                    <td>${PORT_MAPPING[port]}</td>
+                    <td><a href="/setport?portid=${parseInt(port) + 1}&state=${json_res.state[port] ? "0" : "1"}&speed=${json_res.spd_cfg[port]}&flowcontrol=${json_res.fc_cfg[port]}">${["Disabled", "Enabled"][json_res.state[port]]}<a/></td>
+                    <td>${SPEED[json_res.spd_cfg[port]]} (${SPEED[json_res.spd_act[port]]})</td>
+                    <td>${FLOW[json_res.fc_cfg[port]]} (${FLOW[json_res.fc_act[port]]})</td>
+                </tr>`
+            }
+            out += `
+                    </table>
+                </body>
+            </html>`
+            res.end(out)
+        }
+    } else {
+        res.end()
     }
 });
 
